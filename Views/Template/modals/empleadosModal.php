fe273a713f91e9bd8c790d4ab00b76e894867042
NicolasM <nicolaschompi@gmail.com>
2025-04-19 19:02:31 -0500

    Rediseño de Modal Empleados

--- Views/Template/modals/empleadosModal.php
+++ Views/Template/modals/empleadosModal.php
@@ -2,8 +2,8 @@
 <div class="modal fade" id="crearEmpleadoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Crear usuario</h1>
+            <div class="modal-header bg-gradient-dark">
+                <h1 class="modal-title text-light fs-5" id="exampleModalLabel">Crear usuario</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
